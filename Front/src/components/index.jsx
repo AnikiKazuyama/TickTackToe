c924cc35e851545cd6837a065ec444ca77cc283d
Unknown <chilikovvadim@gmail.com>
2018-05-02 20:20:51 +0300

    Редирект с главной на /aut

--- Front/src/components/index.jsx
+++ Front/src/components/index.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link, Redirect } from "react-router-dom";
 
 import AuthContainer from '../containers/AuthContainer';
 
@@ -12,16 +12,9 @@ export default class Root extends React.Component{
     render(){
         return(
             <Switch>
-                <Route exact path = '/' render = { () => {
-                    return(
-                        <div>
-                            <div>Добро пожаловать в игру крестики нолики, для продолжения вам необходимо авторизироваться</div>
-                            <Link to = '/auth'>Войти</Link>
-                            <Link to = '/auth/registration'>Зарегистрироваться</Link>
-                        </div>  
-                    );
-                }}/>
-                <Route path = '/auth' component = { AuthContainer } />
+                <Route path = '/auth' component = { AuthContainer }/>
+                <Route path = '/user' render = {() => {return <div>user</div>} } />
+                <Redirect from = '/' to ='/auth' />
             </Switch>
         );
     }
